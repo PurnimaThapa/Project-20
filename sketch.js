2493af6a8fcaaa4ef8782537abd2fc2145a163a6
PurnimaThapa <76543949+PurnimaThapa@users.noreply.github.com>
2021-01-19 18:52:40 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var car, wall
+var speed, weight
+
+function setup() {
+  createCanvas(1600,400);
+  car = createSprite(100, 200, 50, 50);
+  car.velocityX=speed
+  wall = createSPrite(1500,200,60,height/2)
+  speed=random(55,90)
+  weight=random(400,1500)
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
